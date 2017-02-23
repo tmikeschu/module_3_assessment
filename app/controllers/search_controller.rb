d24bea0f08c99c943c859000af014a2fb017f68a
Mike Schutte <tmikeschutte@gmail.com>
2017-02-23 10:38:40 -0700

    Develop presenter, service, and poro
    All in search controller file for now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,44 @@
 class SearchController < ApplicationController
   def index
-    @presenter = Presenter.present(params[:q])
+    @presenter = SearchPresenter.present(params[:q])
+  end
+end
+
+class SearchPresenter
+  attr_reader :zipcode
+
+  def initialize(zipcode)
+    @zipcode = zipcode
+    @stores  = Store.near(zipcode)
+  end
+
+  def stores_count
+    @stores.count
+  end
+
+  def self.present(zipcode)
+    new(zipcode)
+  end
+end
+
+class Store
+  def initialize(store_attrs)
+    @name     = store_attrs[:longName]
+    @type     = store_attrs[:storeType]
+    @city     = store_attrs[:city]
+    @phone    = store_attrs[:phone]
+    @distance = store_attrs[:distance]
+  end
+
+  def self.near(zipcode)
+    BestBuyStoreService.near(zipcode).each do |store|
+      new(store)
+    end
+  end
+end
+
+class BestBuyStoreService
+  def self.near(zipcode)
+
   end
 end
